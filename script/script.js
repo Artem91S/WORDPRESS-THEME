850a461f7064ba6aecc4525191070350938c26d2
Артём Ситников <sitnikov.artem91@gmail.com>
2023-01-17 12:25:42 +0200

    fixed comments of menter

--- script/script.js
+++ script/script.js
@@ -1,7 +1,5 @@
 /////css
 const input = document.getElementById("input");
-console.log(input);
-
 input.addEventListener("click", color);
 let count=1;
 function color(e){
@@ -21,26 +19,35 @@ else{
 }
 }
 // ////block Our Services
-const nameOfButtons =Array.from(document.querySelector(".click").children);
+let selectButton =document.querySelector('.clikc-button-color');
 const buttonDiscription = Array.from(document.querySelector(".information").children);
-const clickAction = function(event){
-    const nameOfClick = event.currentTarget.dataset.name;
-    buttonDiscription.forEach(element => {
-        element.classList.remove("click-on-button");
-        if(element.getAttribute("show-info") === nameOfClick){
-            element.classList.add("click-on-button")
-        }
-    });
-    nameOfButtons.forEach(buttons => {
-        buttons.classList.remove("clikc-button-color");
-        if(buttons.getAttribute("data-name") === nameOfClick){
-            buttons.classList.add("clikc-button-color")
-        }
-    });
+let btnClick = document.querySelector(".click");
+btnClick.addEventListener("click", e=>{
+let targetClick =e.target;
+let nameOfBtnClick =e.target.dataset.name;
+buttonDiscription.forEach(discription =>{
+    if(discription.getAttribute('show-info') === nameOfBtnClick ){
+        discription.classList.add("click-on-button");
+    }
+    else{
+        discription.classList.remove("click-on-button");
+    }
+})
+activeColor(targetClick);
+})
+
+function activeColor(clickname) {
+ 
+    if(selectButton){
+        
+        selectButton.classList.remove("clikc-button-color")  
+    }
+
+    selectButton = clickname;
+    selectButton.classList.add("clikc-button-color")  
+
 }
-nameOfButtons.forEach(button => {
-    button.addEventListener("click", clickAction)
-});
+
 ///////Our Amazing Work
 const block =document.querySelector('.photo-of-works')
 const buttonLoadImg = document.querySelector(".load-img");
@@ -201,7 +208,7 @@ arr.forEach(key=>{
     <div class="creat-design">
     <div class="icons">
     <svg class="combined" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M13.9131 2.72817L12.0948 0.891285C11.2902 0.0808612 9.98305 0.0759142 9.17681 0.882615L7.15921 2.89256C6.35161 3.69885 6.34818 5.01032 7.15051 5.82074L8.30352 4.68897C8.18678 4.32836 8.33041 3.9153 8.61593 3.62946L9.89949 2.35187C10.3061 1.94624 10.9584 1.94913 11.3595 2.35434L12.4513 3.45805C12.8528 3.86283 12.8511 4.51713 12.447 4.92318L11.1634 6.20241C10.8918 6.47296 10.4461 6.62168 10.1002 6.52626L8.97094 7.65887C9.77453 8.47177 11.0803 8.47466 11.8889 7.66837L13.9039 5.65924C14.7141 4.85254 14.7167 3.53983 13.9131 2.72817ZM6.52613 10.0918C6.62191 10.4441 6.46857 10.8997 6.19093 11.1777L4.99227 12.3752C4.58074 12.7845 3.91595 12.7833 3.50671 12.369L2.39297 11.2475C1.98465 10.8349 1.98729 10.1633 2.39824 9.75473L3.59804 8.55769C3.89032 8.26607 4.31044 8.12025 4.67711 8.24375L5.83354 7.0715C5.01493 6.2462 3.68249 6.24207 2.86059 7.06324L0.915197 9.0042C0.0922615 9.8266 0.0883685 11.1629 0.90651 11.9886L2.75817 13.8618C3.57595 14.6846 4.90724 14.6912 5.73111 13.8701L7.67649 11.9287C8.49852 11.1054 8.5024 9.77166 7.68553 8.9443L6.52613 10.0918ZM6.25787 9.56307C5.98013 9.84189 5.53427 9.84105 5.26179 9.55812C4.98792 9.27434 4.9901 8.82039 5.26613 8.53993L8.59075 5.16109C8.86679 4.88227 9.31174 4.88311 9.58513 5.16398C9.86048 5.44569 9.85876 5.90088 9.5817 6.18299L6.25787 9.56307Z" fill="#1FDAB5"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M13.9131 2.72817L12.0948 0.891285C11.2902 0.0808612 9.98305 0.0759142 9.17681 0.882615L7.15921 2.89256C6.35161 3.69885 6.34818 5.01032 7.15051 5.82074L8.30352 4.68897C8.18678 4.32836 8.33041 3.9153 8.61593 3.62946L9.89949 2.35187C10.3061 1.94624 10.9584 1.94913 11.3595 2.35434L12.4513 3.45805C12.8528 3.86283 12.8511 4.51713 12.447 4.92318L11.1634 6.20241C10.8918 6.47296 10.4461 6.62168 10.1002 6.52626L8.97094 7.65887C9.77453 8.47177 11.0803 8.47466 11.8889 7.66837L13.9039 5.65924C14.7141 4.85254 14.7167 3.53983 13.9131 2.72817ZM6.52613 10.0918C6.62191 10.4441 6.46857 10.8997 6.19093 11.1777L4.99227 12.3752C4.58074 12.7845 3.91595 12.7833 3.50671 12.369L2.39297 11.2475C1.98465 10.8349 1.98729 10.1633 2.39824 9.75473L3.59804 8.55769C3.89032 8.26607 4.31044 8.12025 4.67711 8.24375L5.83354 7.0715C5.01493 6.2462 3.68249 6.24207 2.86059 7.06324L0.915197 9.0042C0.0922615 9.8266 0.0883685 11.1629 0.90651 11.9886L2.75817 13.8618C3.57595 14.6846 4.90724 14.6912 5.73111 13.8701L7.67649 11.9287C8.49852 11.1054 8.5024 9.77166 7.68553 8.9443L6.52613 10.0918ZM6.25787 9.56307C5.98013 9.84189 5.53427 9.84105 5.26179 9.55812C4.98792 9.27434 4.9901 8.82039 5.26613 8.53993L8.59075 5.16109C8.86679 4.88227 9.31174 4.88311 9.58513 5.16398C9.86048 5.44569 9.85876 5.90088 9.5817 6.18299L6.25787 9.56307Z" fill="#FFFF"/>
     </svg><span class="squer"><span></span></span></div><div><h4>creative design</h4>
     <p>${key.name}</p>
     </div>
@@ -217,12 +224,12 @@ let counter =12;
 buttonLoadImg.addEventListener("click", e => {
     counter+=12; 
     buttonLoadImg.style.display ="none";
-    showPhoto("all")
+    showPhoto("all");
 }) 
 /////появление фото
 function showPhoto(event) {
     const ourWorkClick = typeof event == "string"?event:event.currentTarget.dataset.item;
-    if(ourWorkClick == "all" && counter < 13){
+    if(ourWorkClick == "all" && counter < 24){
         buttonLoadImg.style.display ="flex";
     }
     else{
